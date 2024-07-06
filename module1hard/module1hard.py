0c39043a99cf05eb0d79725977e90166e779529d
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-07-06 15:31:52 +0300

    Update module1hard.py

--- module1hard/module1hard.py
+++ module1hard/module1hard.py
@@ -1,7 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {"Johnny", "Bilbo", "Steve", "Khendrik", "Aaron"}
 sorted_students_list = sorted(students)
-print(type(sorted_students_list))
 # students_dict = {
 #     sorted_students_list[0]: sum(grades[0]) / len(grades[0]),
 #     sorted_students_list[1]: sum(grades[1]) / len(grades[1]),
