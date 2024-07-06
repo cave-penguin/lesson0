f4169123674931f73f710010b36fdb0a4b2a1594
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-07-06 10:13:14 +0300

    Update homework6.py

--- homework6/homework6.py
+++ homework6/homework6.py
@@ -1,7 +1,7 @@
 my_dict = {"Name": "Micky", "Age": 42}
 print("Dict:", my_dict)
 print("Existing value:", my_dict["Age"])
-print(my_dict.get("Country", "Not existing value: None"))
+print(my_dict.get("Country", "Not existing value"))
 my_dict.update({"Planet": "Earth", "Post index": 123321})
 name = my_dict.pop("Name")
 print("Deleted value:", name)
