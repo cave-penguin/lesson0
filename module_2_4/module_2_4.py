8b812995f298073e80a2aaf26f0ea21f3f48772a
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-07-10 18:51:19 +0300

    Update module_2_4.py

--- module_2_4/module_2_4.py
+++ module_2_4/module_2_4.py
@@ -4,19 +4,6 @@ not_primes = []
 
 # First solution
 
-# for i in numbers[1:]:
-#     counter = 0
-#     for j in numbers[1 : numbers.index(i)]:
-#         if i % j == 0:
-#             counter += 1
-#             break
-#     if counter == 0:
-#         primes += [i]
-#     else:
-#         not_primes += [i]
-
-# Second solution
-
 for i in numbers[1:]:
     is_prime = True
     for j in numbers[1 : numbers.index(i)]:
@@ -29,6 +16,42 @@ for i in numbers[1:]:
         primes += [i]
     else:
         not_primes += [i]
+
+# Second solution
+
+# for i in numbers:
+#     if i != 0 or i != 1:
+#         is_prime = True
+#         half_list = numbers[1 : numbers.index(i) // 2]
+#         for j in half_list:
+#             if i % j == 0:
+#                 is_prime = False
+#                 break
+#             else:
+#                 is_prime = True
+#         if is_prime:
+#             primes += [i]
+#         else:
+#             not_primes += [i]
+#     else:
+#         break
+
+
+# Third solution
+
+
+# for i in numbers[1:]:
+#     counter = 0
+#     for j in numbers[1 : numbers.index(i)]:
+#         if i % j == 0:
+#             counter += 1
+#             break
+#     if counter == 0:
+#         primes += [i]
+#     else:
+#         not_primes += [i]
+
+
 
 print("Primes:", primes)
 print("Not primes:", not_primes)
