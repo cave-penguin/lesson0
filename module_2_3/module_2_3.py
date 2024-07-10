5cff66c8e88af67a7b900663bd18874eef94ca09
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-07-10 18:26:03 +0300

    Update module_2_3.py

--- module_2_3/module_2_3.py
+++ module_2_3/module_2_3.py
@@ -6,9 +6,7 @@ while i < len(my_list):
     if my_list[i] > 0:
         print(my_list[i])
         i += 1
-        continue
     elif my_list[i] == 0:
         i += 1
-        continue
     else:
         break
