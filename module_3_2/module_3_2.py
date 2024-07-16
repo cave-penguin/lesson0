8b0d059d0774a23df3ba7325e72b190012517d6c
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-07-16 17:20:27 +0300

    Update module_3_2.py

--- module_3_2/module_3_2.py
+++ module_3_2/module_3_2.py
@@ -3,14 +3,8 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
         "@" not in recipient
         or "@" not in sender
         or not (
-            recipient.endswith(".com")
-            or recipient.endswith(".ru")
-            or recipient.endswith(".net")
-        )
-        or not (
-            sender.endswith(".com") 
-            or sender.endswith(".ru") 
-            or sender.endswith(".net")
+            recipient.endswith((".com", ".ru", ".net"))
+            and sender.endswith((".com", ".ru", ".net"))
         )
     ):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
