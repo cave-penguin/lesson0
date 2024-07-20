30082425e804c5840944696c8928bab38758441e
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-07-20 23:38:40 +0300

    Update module3hard.py

--- module3hard/module3hard.py
+++ module3hard/module3hard.py
@@ -7,25 +7,30 @@ data_structure = [
 ]
 
 
-def calculate_structure_sum(*args):
+def calculate_structure_sum(args):
     summa = 0
-    for arg in args:
-        if isinstance(arg, list) or isinstance(arg, set) or isinstance(arg, tuple):
-            summa += calculate_structure_sum(*arg)
-        if isinstance(arg, dict):
-            for key, value in arg.items():
-                summa += len(key)
-                if isinstance(value, int):
-                    summa += value
-                elif isinstance(value, str):
-                    summa += len(value)
-                else:
-                    summa += calculate_structure_sum(*value)
-        if isinstance(arg, int):
-            summa += arg
-        if isinstance(arg, str):
-            summa += len(arg)
-
+    if isinstance(args, dict):
+        for key, value in args.items():
+            summa += len(key)
+            if isinstance(value, int):
+                summa += value
+            elif isinstance(value, str):
+                summa += len(value)
+            elif (
+                isinstance(value, list)
+                or isinstance(value, tuple)
+                or isinstance(value, dict)
+                or isinstance(value, set)
+            ):
+                summa += calculate_structure_sum(value)
+    else:
+        for arg in args:
+            if isinstance(arg, int):
+                summa += arg
+            elif isinstance(arg, str):
+                summa += len(arg)
+            else:
+                summa += calculate_structure_sum(arg)
     return summa
 
 
