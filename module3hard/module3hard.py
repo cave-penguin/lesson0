634f0fca3c3502cad8596b683ecf8e4860ffec5f
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-07-23 17:47:49 +0300

    Update module3hard.py

--- module3hard/module3hard.py
+++ module3hard/module3hard.py
@@ -7,27 +7,54 @@ data_structure = [
 ]
 
 
-def calculate_structure_sum(args):
+# First solution
+
+
+# def calculate_structure_sum(args):
+#     summa = 0
+#     if isinstance(args, dict):
+#         for key, value in args.items():
+#             summa += len(key)
+#             if isinstance(value, int):
+#                 summa += value
+#             elif isinstance(value, str):
+#                 summa += len(value)
+#             else:
+#                 summa += calculate_structure_sum(value)
+#     else:
+#         for arg in args:
+#             if isinstance(arg, int):
+#                 summa += arg
+#             elif isinstance(arg, str):
+#                 summa += len(arg)
+#             else:
+#                 summa += calculate_structure_sum(arg)
+#     return summa
+
+
+# Second solution
+
+
+def calculate_structure_sum(*args):
     summa = 0
-    if isinstance(args, dict):
-        for key, value in args.items():
-            summa += len(key)
-            if isinstance(value, int):
-                summa += value
-            elif isinstance(value, str):
-                summa += len(value)
-            else:
-                summa += calculate_structure_sum(value)
-    else:
-        for arg in args:
-            if isinstance(arg, int):
-                summa += arg
-            elif isinstance(arg, str):
-                summa += len(arg)
-            else:
-                summa += calculate_structure_sum(arg)
+    for arg in args:
+        if isinstance(arg, int):
+            summa += arg
+        elif isinstance(arg, str):
+            summa += len(arg)
+        elif isinstance(arg, dict):
+            for key, value in arg.items():
+                summa += len(key)
+                if isinstance(value, int):
+                    summa += value
+                elif isinstance(value, str):
+                    summa += len(value)
+                else:
+                    summa += calculate_structure_sum(*value)
+        else:
+            summa += calculate_structure_sum(*arg)
     return summa
 
 
-result = calculate_structure_sum(*data_structure)
+result = calculate_structure_sum(data_structure)
 print(result)
