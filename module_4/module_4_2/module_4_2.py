20eacd7996cc1d1e975ca388e35d022d2144a0ee
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-07-31 20:46:09 +0300

    Create module_4_2.py

---
+++ module_4/module_4_2/module_4_2.py
@@ -0,0 +1,10 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+
+
+if __name__ == '__main__':
+    test_function()
+    inner_function()      # name 'inner_function' is not defined
