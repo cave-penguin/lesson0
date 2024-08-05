a573482acb5582d0000728d252350e42e8a4f897
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-08-05 19:33:33 +0300

    Update module_5_1.py

--- module_5/module_5_1/module_5_1.py
+++ module_5/module_5_1/module_5_1.py
@@ -4,7 +4,9 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-        if 0 < new_floor <= self.number_of_floors:
+        if new_floor == 0:
+            print("Вы на первом этаже")
+        elif 0 < new_floor <= self.number_of_floors:
             for floor in range(1, new_floor + 1):
                 print(floor)
         else:
@@ -13,9 +15,13 @@ class House:
 
 h1 = House("ЖК Горский", 18)
 h2 = House("Домик в деревне", 2)
+h3 = House("ЖК Какое-то название", 9)
 h1.go_to(5)
-h1.go_to(0)
-h1.go_to(20)
-h2.go_to(2)
-h2.go_to(-2)
-h2.go_to(1)
+h2.go_to(10)
+
+h3.go_to(0)
+h3.go_to(20)
+h3.go_to(2)
+h3.go_to(-2)
+h3.go_to(1)
+h3.go_to(9)
