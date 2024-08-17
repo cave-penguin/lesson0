6fe375e97a7b69606b4c53e53f17874664037805
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-08-17 15:12:57 +0300

    Update module_6_1.py

--- module_6/module_6_1/module_6_1.py
+++ module_6/module_6_1/module_6_1.py
@@ -1,11 +1,17 @@
-# First solution
-
 class Animal:
     alive = True
     fed = False
 
     def __init__(self, name):
         self.name = name
+
+    def eat(self, food):
+        if food.edible:
+            self.fed = True
+            print(f"{self.name} съел {food.name}")
+        else:
+            self.alive = False
+            print(f"{self.name} не стал есть {food.name}")
 
 
 class Plant:
@@ -16,23 +22,11 @@ class Plant:
 
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f"{self.name} съел {food.name}")
-        else:
-            self.alive = False
-            print(f"{self.name} не стал есть {food.name}")
+    pass
 
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f"{self.name} съел {food.name}")
-        else:
-            self.alive = False
-            print(f"{self.name} не стал есть {food.name}")
+    pass
 
 
 class Flower(Plant):
@@ -41,47 +35,6 @@ class Flower(Plant):
 
 class Fruit(Plant):
     edible = True
-
-
-#  Second solution
-
-# class Animal:
-#     alive = True
-#     fed = False
-
-#     def __init__(self, name):
-#         self.name = name
-
-#     def eat(self, food):
-#         if food.edible:
-#             self.fed = True
-#             print(f"{self.name} съел {food.name}")
-#         else:
-#             self.alive = False
-#             print(f"{self.name} не стал есть {food.name}")
-
-
-# class Plant:
-#     edible = False
-
-#     def __init__(self, name):
-#         self.name = name
-
-
-# class Mammal(Animal):
-#     pass
-
-
-# class Predator(Animal):
-#     pass
-
-
-# class Flower(Plant):
-#     pass
-
-
-# class Fruit(Plant):
-#     edible = True
 
 
 a1 = Predator("Волк с Уолл-Стрит")
