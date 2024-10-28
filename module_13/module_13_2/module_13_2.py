1fa337677c923e2b590b7d55f47bf45ea03e2c59
Mikhail <76682929+cave-penguin@users.noreply.github.com>
2024-10-28 19:46:51 +0300

    Update module_13_2.py

--- module_13/module_13_2/module_13_2.py
+++ module_13/module_13_2/module_13_2.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
 
-api = "7887074932:AAGWX-FV2p_foXbF05fSXiAHuZ6LN8_zo8g"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
